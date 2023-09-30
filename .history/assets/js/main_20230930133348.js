ab151f82b885a54a03527f790b01bee2d18b7daf
PhamThuan <phamminhthuan912@gmail.com>
2023-10-01 01:14:22 +0700

    done

---
+++ .history/assets/js/main_20230930133348.js
@@ -0,0 +1,233 @@
+window.addEventListener("load", function () {
+    // const daysText = document.querySelector("#days");
+    // const hoursText = document.querySelector("#hours");
+    // const minutesText = document.querySelector("#minutes");
+    // const secondsText = document.querySelector("#seconds");
+
+    // function setTimer(
+    //     dayInput,
+    //     monthInput,
+    //     dateInput,
+    //     yearInput,
+    //     hourInput,
+    //     minuteInput,
+    //     gmtInput
+    // ) {
+    //     let currentTime = new Date().getTime();
+    //     const endTime = new Date(
+    //         yearInput,
+    //         getMonthIndex(monthInput),
+    //         dateInput,
+    //         hourInput,
+    //         minuteInput
+    //     ).getTime();
+
+    //     if (isNaN(endTime) || endTime - currentTime <= 0) return;
+    //     setInterval(changeTimer, 500);
+    //     changeTimer();
+
+    //     function changeTimer() {
+    //         let days, hours, minutes, seconds;
+    //         const now = new Date();
+    //         let startTime = now.getTime();
+    //         let countDown = parseInt((endTime - startTime) / 1000);
+    //         if (countDown > 0) {
+    //             days = parseInt(countDown / 86400);
+    //             countDown = countDown % 86400;
+    //             hours = parseInt(countDown / 3600);
+    //             countDown = countDown % 3600;
+    //             minutes = parseInt(countDown / 60);
+    //             countDown = countDown % 60;
+    //             seconds = parseInt(countDown);
+
+    //             daysText.textContent = `${days}`.slice(-2);
+    //             hoursText.textContent = `${hours}`.slice(-2);
+    //             minutesText.textContent = `${minutes}`.slice(-2);
+    //             secondsText.textContent = `${seconds}`.slice(-2);
+    //         }
+    //     }
+
+    //     function getMonthIndex(monthName) {
+    //         const months = [
+    //             "Jan",
+    //             "Feb",
+    //             "Mar",
+    //             "Apr",
+    //             "May",
+    //             "Jun",
+    //             "Jul",
+    //             "Aug",
+    //             "Sep",
+    //             "Oct",
+    //             "Nov",
+    //             "Dec",
+    //         ];
+    //         return months.indexOf(monthName);
+    //     }
+    // }
+
+    // const formTimer = document.querySelector(".form-timer");
+    // formTimer.addEventListener("submit", function (e) {
+    //     e.preventDefault();
+    //     const dayInput = this.elements["day"].value;
+    //     const monthInput = this.elements["month"].value;
+    //     const dateInput = parseInt(this.elements["date"].value);
+    //     const yearInput = parseInt(this.elements["year"].value);
+    //     const hourInput = parseInt(this.elements["hour"].value);
+    //     const minuteInput = parseInt(this.elements["minute"].value);
+    //     const gmtInput = this.elements["GMT"].value;
+    //     setTimer(
+    //         dayInput,
+    //         monthInput,
+    //         dateInput,
+    //         yearInput,
+    //         hourInput,
+    //         minuteInput,
+    //         gmtInput
+    //     );
+    // });
+
+    // xử lý nhạc
+    const btnPlay = document.querySelector(".player-play");
+    const btnNext = document.querySelector(".player-next");
+    const btnPrev = document.querySelector(".player-prev");
+    const playerImg = document.querySelector(".player-image");
+    const bar = document.querySelector(".bar");
+    const currentTimeText = document.querySelector(".player-remaining");
+    const durationTimeText = document.querySelector(".player-duration");
+    const nameMusic = document.querySelector(".player-nameMusic");
+
+    let playing = true;
+    const song = document.querySelector("#song");
+    btnPlay.addEventListener("click", playMusic);
+    function playMusic(e) {
+        if (playing) {
+            // khi nhạc đang phát thì đổi thành false (tắt nhạc)
+            playing = false;
+            btnPlay.classList.remove("fa-play");
+            btnPlay.classList.add("fa-pause");
+            playerImg.classList.add("is-playing");
+            song.play();
+            const name = song.getAttribute("src");
+            nameMusic.textContent = name;
+        } else {
+            // khi nhạc đang tắt thì đổi thành true (phát nhạc)
+            playing = true;
+            btnPlay.classList.add("fa-play");
+            btnPlay.classList.remove("fa-pause");
+            playerImg.classList.remove("is-playing");
+            song.pause();
+            const name = song.getAttribute("src");
+            nameMusic.textContent = name;
+        }
+    }
+
+    // let listMusic = [
+    //     "mien-man.mp3",
+    //     "vetthuong.mp3",
+    //     "lancuoi.mp3",
+    //     "neumotngaychungtakhongcongap.mp3",
+    //     "thang11coem.mp3",
+    //     "tinhcoyeuem.mp3",
+    // ];
+    let index = 0;
+
+    btnNext.addEventListener("click", function () {
+        playing = true;
+        index++;
+        if (index > listMusicRender.songs.length - 1) {
+            index = 0;
+        }
+        song.setAttribute("src", listMusicRender.songs[index].path);
+        playMusic();
+    });
+
+    btnPrev.addEventListener("click", function () {
+        playing = true;
+        index--;
+        if (index < 0) {
+            index = listMusicRender.songs.length - 1;
+        }
+        song.setAttribute("src", listMusicRender.songs[index].path);
+        playMusic();
+    });
+
+    function checkTimer() {
+        const { duration, currentTime } = song;
+        bar.max = song.duration;
+        bar.value = song.currentTime;
+        currentTimeText.textContent = formatTimeMusic(currentTime);
+        if (!duration) {
+            durationTimeText.textContent = "0:00";
+        } else {
+            durationTimeText.textContent = formatTimeMusic(duration);
+        }
+    }
+
+    function formatTimeMusic(number) {
+        const minutes = parseInt(Math.floor(number / 60));
+        const seconds = parseInt(Math.floor(number - minutes * 60));
+        return `0${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+    }
+    const timer = setInterval(checkTimer, 1000); // chạy hàm sau mỗi lần 1s để nó reset tăng lên thời gian hiện tại (currentTime)
+    bar.addEventListener("change", function (e) {
+        song.currentTime = bar.value;
+    });
+
+    checkTimer();
+
+    const listMusicRender = {
+        // array object
+        songs: [
+            {
+                name: "Lần cuối",
+                singer: "Ngọt",
+                path: "./assets/files/lancuoi.mp3",
+                img: "./assets/img/mthis.mp3",
+            },
+            {
+                name: "Miên man",
+                singer: "Minh Huy",
+                path: "./assets/files/mien-man.mp3",
+                img: "./assets/img/mthis.mp3",
+            },
+            {
+                name: "Nếu một ngày chúng ta không còn gặp",
+                singer: "Dick",
+                path: "./assets/files/neumotngaychungtakhongcongap.mp3",
+                img: "./assets/img/mthis.mp3",
+            },
+            {
+                name: "Tháng 11 có em",
+                singer: "Chả biết ca sĩ",
+                path: "./assets/files/thang11coem.mp3",
+                img: "./assets/img/mthis.mp3",
+            },
+            {
+                name: "Vết thương",
+                singer: "Ngọt",
+                path: "./assets/files/vetthuong.mp3",
+                img: "./assets/img/mthis.mp3",
+            },
+        ],
+
+        render: function () {
+            const htmls = this.songs.map((song, index) => {
+                // Xây dựng HTML cho từng bài hát ở đây
+                // Ví dụ:
+                return `
+                    <div class="song ${
+                        index === this.currentIndex ? "active">
+                        <h2>${song.name}</h2>
+                        <p>${song.singer}</p>
+                        <img src="${song.img}" alt="${song.name}" />
+                        <audio src="${song.path}" controls></audio>
+                    </div>
+                `;
+            });
+            const songsContainer = document.querySelector(".songs-container");
+            songsContainer.innerHTML = htmls.join("");
+        },
+    };
+    listMusicRender.render();
+});
---
+++ .history/assets/js/main_20230930180317.js
@@ -0,0 +1,235 @@
+window.addEventListener("load", function () {
+    const daysText = document.querySelector("#days");
+    const hoursText = document.querySelector("#hours");
+    const minutesText = document.querySelector("#minutes");
+    const secondsText = document.querySelector("#seconds");
+
+    function setTimer(
+        dayInput,
+        monthInput,
+        dateInput,
+        yearInput,
+        hourInput,
+        minuteInput,
+        gmtInput
+    ) {
+        let currentTime = new Date().getTime();
+        const endTime = new Date(
+            yearInput,
+            getMonthIndex(monthInput),
+            dateInput,
+            hourInput,
+            minuteInput
+        ).getTime();
+
+        if (isNaN(endTime) || endTime - currentTime <= 0) return;
+        setInterval(changeTimer, 500);
+        changeTimer();
+
+        function changeTimer() {
+            let days, hours, minutes, seconds;
+            const now = new Date();
+            let startTime = now.getTime();
+            let countDown = parseInt((endTime - startTime) / 1000);
+            if (countDown > 0) {
+                days = parseInt(countDown / 86400);
+                countDown = countDown % 86400;
+                hours = parseInt(countDown / 3600);
+                countDown = countDown % 3600;
+                minutes = parseInt(countDown / 60);
+                countDown = countDown % 60;
+                seconds = parseInt(countDown);
+
+                daysText.textContent = `${days}`.slice(-2);
+                hoursText.textContent = `${hours}`.slice(-2);
+                minutesText.textContent = `${minutes}`.slice(-2);
+                secondsText.textContent = `${seconds}`.slice(-2);
+            }
+        }
+
+        function getMonthIndex(monthName) {
+            const months = [
+                "Jan",
+                "Feb",
+                "Mar",
+                "Apr",
+                "May",
+                "Jun",
+                "Jul",
+                "Aug",
+                "Sep",
+                "Oct",
+                "Nov",
+                "Dec",
+            ];
+            return months.indexOf(monthName);
+        }
+    }
+
+    const formTimer = document.querySelector(".form-timer");
+    formTimer.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const dayInput = this.elements["day"].value;
+        const monthInput = this.elements["month"].value;
+        const dateInput = parseInt(this.elements["date"].value);
+        const yearInput = parseInt(this.elements["year"].value);
+        const hourInput = parseInt(this.elements["hour"].value);
+        const minuteInput = parseInt(this.elements["minute"].value);
+        const gmtInput = this.elements["GMT"].value;
+        setTimer(
+            dayInput,
+            monthInput,
+            dateInput,
+            yearInput,
+            hourInput,
+            minuteInput,
+            gmtInput
+        );
+    });
+
+    const container = document.querySelector(".songs-container");
+    let currentSongIndex = 0; // Đặt chỉ mục ban đầu thành 0 hoặc chỉ mục của bài hát mặc định
+    const listMusicRender = {
+        songs: [
+            {
+                name: "Lần cuối",
+                singer: "Ngọt",
+                path: "./assets/files/lan-cuoi.mp3",
+                img: "./assets/img/mthis.png",
+            },
+            {
+                name: "Miên man",
+                singer: "Minh Huy",
+                path: "./assets/files/mien-man.mp3",
+                img: "./assets/img/mthis.png",
+            },
+            {
+                name: "Nếu một ngày chúng ta không còn gặp",
+                singer: "Dick",
+                path: "./assets/files/neu-mot-ngay-chung-ta-khong-con-gap.mp3",
+                img: "./assets/img/mthis.png",
+            },
+            {
+                name: "Tháng 11 có em",
+                singer: "Thanh Hòa",
+                path: "./assets/files/thang-11-co-em",
+                img: "./assets/img/mthis.png",
+            },
+            {
+                name: "Tình cờ yêu em",
+                singer: "Kunn Đức Nam",
+                path: "./assets/files/tinh-coyeuem.mp3",
+                img: "./assets/img/mthis.png",
+            },
+            {
+                name: "Vết thương",
+                singer: "Fishy",
+                path: "./assets/files/vetthuong.mp3",
+                img: "./assets/img/mthis.png",
+            },
+            // Thêm nhiều bài hát khác ở đây
+        ],
+
+        render: function () {
+            const song = this.songs[currentSongIndex];
+            const htmls = `<div class="song" data-index="${currentSongIndex}">
+                    <h2>${song.name}</h2>
+                    <p>${song.singer}</p>
+                    <img src="${song.img}" alt="${song.name}" class="img" />
+                    <audio src="${song.path}" controls id="song" style="display: none"></audio>
+                </div>
+            `;
+            container.innerHTML = htmls;
+        },
+    };
+
+    listMusicRender.render();
+    const btnPlay = document.querySelector(".player-play");
+    const btnNext = document.querySelector(".player-next");
+    const btnPrev = document.querySelector(".player-prev");
+    const bar = document.querySelector(".bar");
+    const currentTimeText = document.querySelector(".player-remaining");
+    const durationTimeText = document.querySelector(".player-duration");
+    const song = document.querySelector("#song");
+
+    let playing = false;
+    const songs = listMusicRender.songs;
+
+    btnPlay.addEventListener("click", playMusic);
+
+    function playMusic(e) {
+        if (playing) {
+            playing = false;
+            btnPlay.classList.remove("fa-pause");
+            btnPlay.classList.add("fa-play");
+            song.pause();
+        } else {
+            playing = true;
+            btnPlay.classList.remove("fa-play");
+            btnPlay.classList.add("fa-pause");
+            song.play();
+        }
+    }
+
+    btnNext.addEventListener("click", function () {
+        handleChangeMusic(1);
+    });
+
+    btnPrev.addEventListener("click", function () {
+        handleChangeMusic(-1);
+    });
+
+    song.addEventListener("ended", function() {
+        handleChangeMusic(1)
+    })
+
+    function handleChangeMusic(dir) {
+        if (dir === 1) {
+            currentSongIndex++;
+            if (currentSongIndex >= songs.length) {
+                currentSongIndex = 0;
+            }
+            playSelectedSong(currentSongIndex);
+        } else if (dir === -1) {
+            currentSongIndex--;
+            if (currentSongIndex < 0) {
+                currentSongIndex = songs.length - 1;
+            }
+            playSelectedSong(currentSongIndex);
+        }
+    }
+
+    function playSelectedSong(songIndex) {
+        currentSongIndex = songIndex; // Cập nhật chỉ mục bài hát hiện tại
+        listMusicRender.render(); // Cập nhật giao diện người dùng
+        song.src = songs[currentSongIndex].path;
+        song.play();
+        playing = true;
+        btnPlay.classList.remove("fa-play");
+        btnPlay.classList.add("fa-pause");
+    }
+
+    function checkTimer() {
+        const { duration, currentTime } = song;
+        bar.max = song.duration;
+        bar.value = song.currentTime;
+        currentTimeText.textContent = formatTimeMusic(currentTime);
+        if (!duration) {
+            durationTimeText.textContent = "0:00";
+        } else {
+            durationTimeText.textContent = formatTimeMusic(duration);
+        }
+    }
+
+    function formatTimeMusic(number) {
+        const minutes = parseInt(Math.floor(number / 60));
+        const seconds = parseInt(Math.floor(number - minutes * 60));
+        return `0${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
+    }
+    const timer = setInterval(checkTimer, 1000); // chạy hàm sau mỗi lần 1s để nó reset tăng lên thời gian hiện tại (currentTime)
+    bar.addEventListener("change", function (e) {
+        song.currentTime = bar.value;
+    });
+
+    checkTimer();
+});
